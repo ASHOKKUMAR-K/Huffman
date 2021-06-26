22efcb355ea8f2184776b269d8e966c6ab13bba7
ashok.ai <ashokkumar.datascience@gmail.com>
2021-06-26 15:22:12 +0530

    Huffman Encoding and Decoding Inplemented & Tested

--- huffman.py
+++ huffman.py
@@ -1,27 +1,95 @@
 #!/usr/local/bin/python3
 import sys
 import argparse
-import shutil
+# import shutil
+import json
+
+class Node:
+    def __init__(self, character=None, frequency=None):
+        self.left = None
+        self.right = None
+        self.char = character
+        self.freq = frequency
+        
+    def __str__(self):
+        return "{} : {}".format(self.char, self.freq)
+    
+def get_char_codes(node, char_codes, code):
+    if node.left is None and node.right is None:
+        char_codes[node.char] = code
+    if node.left is not None:
+        get_char_codes(node.left, char_codes, code+"0")
+    if node.right is not None:
+        get_char_codes(node.right, char_codes, code+"1")
+        
+def create_huffman_tree(nodes):
+    while len(nodes) > 1:
+        nodes = sorted(nodes, key=lambda x: x.freq, reverse=True)
+        first_least_node = nodes[-1]
+        second_least_node = nodes[-2]
+        nodes.pop()
+        nodes.pop()
+        new_node = Node(frequency=first_least_node.freq + second_least_node.freq)
+        new_node.left = first_least_node
+        new_node.right = second_least_node
+        nodes.append(new_node)
+    return nodes[0]
 
 
 def encode(input_file, output_file):
 	print("encoding ", input_file, output_file)
 	# write code here
+	with open("story.txt", "r") as in_file:
+		input_string = "".join(in_file.readlines())
+	chars_freq = {}
 
-	# simply copying the file to bypass the actual test.
-	# remove the below lines.
-	if input_file != "" and output_file != "":
-		shutil.copyfile(input_file, output_file)
+	for char in input_string:
+		try:
+			chars_freq[char] += 1
+		except:
+			chars_freq[char] = 1
 
+	nodes = [Node(char, freq) for char, freq in chars_freq.items()]
+
+	root = create_huffman_tree(nodes)
+
+	char_codes = {}
+
+	get_char_codes(root, char_codes, "")
+	# print(char_codes)
+
+	encoded_input = ""
+	for char in input_string:
+		encoded_input += char_codes[char]
+	
+	with open(output_file, "w+") as out:
+		out.write(encoded_input)
+	with open("huffman_tree_codes.json", "w") as code_file:
+		code_chars = {code:char for char, code in char_codes.items()}
+		json.dump(code_chars, code_file)
 
 def decode(input_file, output_file):
 	print("decoding ", input_file, output_file)
 	# write code here
-
-	# simply copying the file to bypass the actual test.
-	# remove the below lines.
-	if input_file != "" and output_file != "":
-		shutil.copyfile(input_file, output_file)
+	with open("huffman_tree_codes.json", "r") as code_file:
+		code_chars = json.load(code_file)
+	with open(input_file, "r") as in_file:
+		encoded_input = in_file.read()
+	
+	decoded_string = ""
+	key = ""
+	for bit in encoded_input:
+		key += bit
+		try:
+			decoded_string += code_chars[key]
+			key = ""
+		except:
+			continue
+	
+	print(decoded_string)
+ 
+	with open(output_file, "w") as out:
+		out.write(decoded_string)
 
 
 def get_options(args=sys.argv[1:]):
--- test.py
+++ test.py
@@ -6,14 +6,12 @@ from huffman import encode, decode
 class TestHuffman(unittest.TestCase):
 	# write all your tests here
 	# function name should be prefixed with 'test'
-
-	def test_encode(self):
-		encode("", "")
-		assert True
-
-	def test_decode(self):
-		decode("", "")
-		assert True
+	def test_encode_decode_1(self):
+		assert self.assertEqual("ASDFGHJKJHGFDERTYUJNBVCFDRGHBVCSDFJMN", decode(encode("ASDFGHJKJHGFDERTYUJNBVCFDRGHBVCSDFJMN")))
+	
+	
+	def test_encode_decode_2(self):
+		assert self.assertEqual("ASDFGHJKJHGFDE sjbkcdvc dbvh! DRGHBVCSDFJMN", decode(encode("ASDFGHJKJHGFDE sjbkcdvc dbvh! DRGHBVCSDFJMN")))
 
 
 if __name__ == '__main__':
